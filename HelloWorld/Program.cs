65a9501159b201f740d11f4f393031ace1b4179b
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-01-05 16:12:48 -0500

    Hello World C# app made

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); //write to console
+            Console.ReadLine(); //hangs the program -> press enter to exit
+        }
+    }
+}
