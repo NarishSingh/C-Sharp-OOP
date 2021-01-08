08b34b2f96b896a98270d5841e773cc1cb548927
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-01-08 15:24:23 -0500

    fizzbuzz ex added

---
+++ Ex2FixxBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex2FixxBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                Console.Write($"{i}");
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write(" fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write(" fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write(" buzz");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
