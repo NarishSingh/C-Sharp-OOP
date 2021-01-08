38775b3f2424a827c2ed6aae4fb0393713adae51
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-01-08 15:12:54 -0500

    Adding factorizer example

---
+++ Ex1Factorizer/Calculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ex1Factorizer
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// Given a number, print the factors
+        /// </summary>
+        /// <param name="number">int</param>
+        public static void PrintFactors(int number)
+        {
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Given a number, print if it is perfect or not
+        /// </summary>
+        /// <param name="number">int</param>
+        public static void IsPerfectNumber(int number)
+        {
+            int sum = 0;
+
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"Is a perfect number: {sum == number}");
+        }
+
+        /// <summary>
+        /// Given a number, print if it is prime or not
+        /// </summary>
+        /// <param name="number">int</param>
+        public static void IsPrimeNumber(int number)
+        {
+            ArrayList fctList = new ArrayList();
+
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    fctList.Add(i);
+                }
+            }
+
+            Console.WriteLine($"Is Prime: {fctList.Count == 2}");
+        }
+    }
+}
---
+++ Ex1Factorizer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ex1Factorizer
+{
+    class Program
+    {
+        /// <summary>
+        /// Prompt the user for an integer.  Make sure they enter a valid integer!
+        /// 
+        /// See the String Input lesson for TryParse() examples
+        /// </summary>
+        /// <returns>the user input as an integer</returns>
+        static int GetNumberFromUser()
+        {
+            int output;
+
+            Console.WriteLine("Enter an integer: ");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out output))
+                {
+                    return output;
+                }
+
+                Console.WriteLine("Not an integer...");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int number = GetNumberFromUser();
+
+            Calculator.PrintFactors(number);
+            Calculator.IsPerfectNumber(number);
+            Calculator.IsPrimeNumber(number);
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+    }
+}
