c44c838e888842f4e55fbff376e9dccba3585f42
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-01-08 21:08:04 -0500

    Class lesson goblin battle method version added

---
+++ ClassesGoblinBattle/Goblin.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * This uses the standard/java-esque way
+ */
+
+using System;
+
+namespace ClassesGoblinBattle
+{
+    public class Goblin
+    {
+        private static Random _rng = new Random();
+        private int _hitPoints;
+        private string _name;
+        private bool _dead;
+
+        /*getters and setters*/
+        public int GetHitPoints()
+        {
+            return _hitPoints;
+        }
+
+        public void SetHitPoints(int hp)
+        {
+            if (hp < 0)
+            {
+                return;
+            }
+            else
+            {
+                _hitPoints = hp;
+            }
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+
+        //getter only for this one
+        public bool IsDead()
+        {
+            return _dead;
+        }
+
+        /*functions*/
+        /// <summary>
+        /// Take a hit
+        /// </summary>
+        /// <param name="dmg">amount of damage received from hit</param>
+        public void Hit(int dmg)
+        {
+            _hitPoints -= dmg;
+
+            Console.WriteLine($"{_name} receives {dmg} damage. {_hitPoints} hp remaining");
+
+            //death handler
+            if (_hitPoints <= 0)
+            {
+                Console.WriteLine($"{_name} has died");
+                _dead = true;
+            }
+        }
+
+        /// <summary>
+        /// Attack a target
+        /// </summary>
+        /// <param name="target">Goblin to attack</param>
+        public void Atk(Goblin target)
+        {
+            int dmg = _rng.Next(5);
+            Console.WriteLine($"{_name} attacks {target.GetName()} for {dmg} damage!");
+            
+            target.Hit(dmg);
+        }
+    }
+}
---
+++ ClassesGoblinBattle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassesGoblinBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*make the goblins battle*/
+            Goblin g1 = new Goblin();
+            g1.SetName("Bob");
+            g1.SetHitPoints(10);
+
+            Goblin g2 = new Goblin();
+            g2.SetName("Tom");
+            g2.SetHitPoints(10);
+
+            int whoseTurn = 1;
+
+            while (!g1.IsDead() && !g2.IsDead())
+            {
+                if (whoseTurn == 1)
+                {
+                    g1.Atk(g2);
+                    whoseTurn = 2;
+                }
+                else
+                {
+                    g2.Atk(g1);
+                    whoseTurn = 1;
+                }
+            }
+
+            Console.WriteLine("The battle has ended!");
+            Console.WriteLine(g1.IsDead()
+                ? $"{g2.GetName()} has won!"
+                : $"{g1.GetName()} has won!");
+        }
+    }
+}
