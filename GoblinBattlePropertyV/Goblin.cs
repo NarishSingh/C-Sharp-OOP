38ae5ea34debd33b2f9b8748dab16d24e7cf54f9
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-01-10 16:24:19 -0500

    Adding goblin battle property version

---
+++ GoblinBattlePropertyV/Goblin.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace GoblinBattlePropertyV
+{
+    public class Goblin
+    {
+        private static Random _rng = new Random();
+
+        //property versions
+        public string Name { get; set; }
+        public bool Dead { get; private set; } //private set so only this class's methods can set it
+
+        private int _hitPoints; //custom setter for property
+
+        public int HitPoints
+        {
+            get => _hitPoints;
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _hitPoints = value;
+                }
+            }
+        }
+
+        //functions
+        /// <summary>
+        /// Take a hit
+        /// </summary>
+        /// <param name="dmg">amount of damage received from hit</param>
+        public void Hit(int dmg)
+        {
+            HitPoints -= dmg;
+            Console.WriteLine($"{Name} receives {dmg} dmg. They have {HitPoints} hp");
+
+            //death handler
+            if (HitPoints <= 0)
+            {
+                Dead = true;
+            }
+        }
+
+        /// <summary>
+        /// Attack a target
+        /// </summary>
+        /// <param name="target">Goblin to attack</param>
+        public void Atk(Goblin target)
+        {
+            int dmg = _rng.Next(5);
+            Console.WriteLine($"{Name} attacks {target.Name} for {dmg} dmg!");
+
+            target.Hit(dmg);
+        }
+    }
+}
---
+++ GoblinBattlePropertyV/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GoblinBattlePropertyV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Goblin g1 = new Goblin();
+            g1.Name = "Bob";
+            g1.HitPoints = 10;
+
+            //obj initializer
+            Goblin g2 = new Goblin {Name = "Tom", HitPoints = 10};
+
+            int whoseTurn = 1;
+
+            while (!g1.Dead && !g2.Dead)
+            {
+                if (whoseTurn == 1)
+                {
+                    g1.Atk(g2);
+                    whoseTurn = 2;
+                }
+                else
+                {
+                    g2.Atk(g1);
+                    whoseTurn = 1;
+                }
+            }
+
+            Console.WriteLine("The battle has ended!");
+            Console.WriteLine(g1.Dead
+                ? $"{g2.Name} has won!"
+                : $"{g1.Name} has won!");
+        }
+    }
+}
