f607ad7d5f9dfd56b05dd5ab30b3d74af19784ee
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-01-10 16:56:01 -0500

    Adding class libraries project

---
+++ ConsoleUtilities/ConsoleUtilities.BLL/UserInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleUtilities.BLL
+{
+    public class UserInput
+    {
+        public static int ReadInt(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                string inp = Console.ReadLine();
+
+                int result;
+                if (int.TryParse(inp, out result))
+                {
+                    return result;
+                }
+
+                Console.WriteLine("Not an integer.");
+            }
+        }
+    }
+}
---
+++ ConsoleUtilities/ConsoleUtilities.UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleUtilities.BLL;
+
+namespace ConsoleUtilities.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = UserInput.ReadInt("Enter your age: ");
+
+            Console.WriteLine($"Age: {age}", age);
+        }
+    }
+}
