e9620499818ff53e8f47942f8ca76f4c5f4442e8
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-01-11 13:03:21 -0500

    Adding Tests lesson Proj

---
+++ Calculator/Calculator.BLL/SimpleMath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator.BLL
+{
+    public class SimpleMath
+    {
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Calculator/Calculator.Tests/SimpleMathTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+using Calculator.BLL;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class SimpleMathTests
+    {
+        private SimpleMath math;
+        
+        [SetUp]
+        public void Init()
+        {
+            math = new SimpleMath();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            
+        }
+        
+        [Test]
+        public void IntDivision()
+        {
+            // SimpleMath math = new SimpleMath();
+            int div = math.Divide(5, 2);
+
+            Assert.AreEqual(2, div);
+        }
+
+        [TestCase(4, 2, 2)]
+        [TestCase(13, 6, 2)]
+        [TestCase(-20, 5, -4)]
+        public void IntDivisionCases(int x, int y, int expected)
+        {
+            // SimpleMath math = new SimpleMath();
+            int actual = math.Divide(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void DivideByZeroTest()
+        {
+            // SimpleMath math = new SimpleMath();
+            Assert.Throws<DivideByZeroException>(() => math.Divide(5, 0));
+        }
+    }
+}
