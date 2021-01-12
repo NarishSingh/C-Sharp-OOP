0105a18792ee920ef20188f5eb14f1aeb449d207
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-01-12 15:33:51 -0500

    cleanup

--- Factorizer2/Factorizer2.Tests/FactorFinderTests.cs
+++ Factorizer2/Factorizer2.Tests/FactorFinderTests.cs
@@ -6,9 +6,9 @@ namespace Factorizer2.Tests
     [TestFixture]
     public class FactorFinderTests
     {
-        [TestCase(4, new int[] {1, 2})]
-        [TestCase(6, new int[] {1, 2, 3})]
-        [TestCase(9, new int[] {1, 3})]
+        [TestCase(4, new[] {1, 2})]
+        [TestCase(6, new[] {1, 2, 3})]
+        [TestCase(9, new[] {1, 3})]
         public void FactorsOfTest(int n, int[] expected)
         {
             int[] actual = FactorFinder.FactorsOf(n);
