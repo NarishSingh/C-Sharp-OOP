a1e6100257431d496b74939ef915cbb940d54b3f
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-01-27 13:56:08 -0500

    Pushing IComparable lesson proj

---
+++ ComparingMovies/ComparingMovies/MovieRatings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ComparingMovies
+{
+    public class MovieRatings: IComparable<MovieRatings>
+    {
+        public string Title { get; set; }
+        public int Rating { get; set; }
+        
+        public int CompareTo(MovieRatings? other)
+        {
+            if (other == null)
+            {
+                return 1; // > 0 b/c null will be added ahead, at end of list
+            }
+
+            return Rating.CompareTo(other.Rating);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Title)}: {Title}, {nameof(Rating)}: {Rating}";
+        }
+    }
+}
---
+++ ComparingMovies/ComparingMovies/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingMovies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<MovieRatings> movies = new List<MovieRatings>
+            {
+                new() {Title = "Her", Rating = 8},
+                new() {Title = "Fletch", Rating = 5},
+                new() {Title = "Superbabies: Baby Geniuses 2", Rating = 9},
+                new() {Title = "Howl's Moving Castle", Rating = 10}
+            };
+            movies.Sort(); //will use the CompareTo override we provided in the class
+
+            foreach (MovieRatings movieRatings in movies)
+            {
+                Console.WriteLine(movieRatings.ToString());
+            }
+        }
+    }
+}
