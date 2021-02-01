258e5d226b2334f9030cfc4c40ad4f3e037aa5b0
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-02-01 00:11:17 -0500

    Cleanup

--- SGBank/SGBank.UI/Program.cs
+++ SGBank/SGBank.UI/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace SGBank.UI
+﻿using SGBank.BLL;
+using SGBank.UI.View;
+
+namespace SGBank.UI
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            BankController c = new BankController(AcctServiceFactory.Create(), new BankView());
+            c.Run();
         }
     }
 }
