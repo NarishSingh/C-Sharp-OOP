4320ccb3aba6c3a28dfee10059095d7e00792453
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-02-02 16:47:31 -0500

    Save Account Test

--- SGBankApp/SGBankApp.Tests/FreeAcctTests.cs
+++ SGBankApp/SGBankApp.Tests/FreeAcctTests.cs
@@ -18,5 +18,23 @@ namespace SGBankApp.Tests
             Assert.IsTrue(a.Success);
             Assert.AreEqual("12345", a.Acct.AcctNum);
         }
+
+        [Test]
+        public void SaveFreeAcctTest()
+        {
+            const decimal dep = 100.00M;
+            AcctService serv = AcctServiceFactory.Create();
+            AcctLookupResponse a = serv.LookupAcct("12345");
+            decimal oldBal = a.Acct.Balance;
+            decimal afterDepBal = a.Acct.Balance + dep;
+
+            AcctDepositResponse rsp = serv.Deposit(a.Acct.AcctNum, dep);
+            
+            Assert.IsNotNull(rsp.Acct);
+            Assert.IsTrue(rsp.Success);
+            Assert.AreEqual(rsp.Deposit, dep);
+            Assert.AreEqual(rsp.OldBalance, oldBal);
+            Assert.AreEqual(rsp.Acct.Balance, afterDepBal);
+        }
     }
 }
