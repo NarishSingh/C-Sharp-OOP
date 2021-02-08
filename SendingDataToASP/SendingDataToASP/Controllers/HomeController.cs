820f4adf5a30ba554210292e3e54c8a4fbb43849
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-02-08 14:30:27 -0500

    Completed data passing lesson proj

--- SendingDataToASP/SendingDataToASP/Controllers/HomeController.cs
+++ SendingDataToASP/SendingDataToASP/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Web;
@@ -100,9 +101,56 @@ namespace SendingDataToASP.Controllers
         /// <summary>
         /// Passes data via the path by binding route data to model obj
         /// </summary>
+        /// <param name="model">ProductSearch model with data scraped by model binder from GET request</param>
         /// <returns>Search Result view with the ProductSearch model data</returns>
         [HttpGet]
         public ActionResult Path3(ProductSearch model)
+        {
+            return View("SearchResult", model);
+        }
+
+        /*REQUEST BODY - FORM*/
+        /// <summary>
+        /// Passes data from form via Request obj's Form NameValue collection field
+        /// </summary>
+        /// <returns>Search Result view with the ProductSearch model data</returns>
+        [HttpPost]
+        public ActionResult Form1()
+        {
+            var model = new ProductSearch
+            {
+                Category = Request.Form["category"],
+                Subcategory = Request.Form["subcategory"]
+            };
+
+            return View("SearchResult", model);
+        }
+
+        /// <summary>
+        /// Passes data from form via method params pulled from request body
+        /// </summary>
+        /// <param name="category">string from form request</param>
+        /// <param name="subcategory">string from form request</param>
+        /// <returns>Search Result view with the ProductSearch model data</returns>
+        [HttpPost]
+        public ActionResult Form2(string category, string subcategory)
+        {
+            var model = new ProductSearch
+            {
+                Category = category,
+                Subcategory = subcategory
+            };
+
+            return View("SearchResult", model);
+        }
+
+        /// <summary>
+        /// Passes data from from via modeling binding
+        /// </summary>
+        /// <param name="model">ProductSearch model with data scraped by model binder from GET request</param>
+        /// <returns>Search Result view with the ProductSearch model data</returns>
+        [HttpPost]
+        public ActionResult Form3(ProductSearch model)
         {
             return View("SearchResult", model);
         }
