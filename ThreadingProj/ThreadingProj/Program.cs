c380667ffcef11ef358d3ab1285e21448fda96ed
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-07-26 15:58:10 -0400

    Local vs shared state

--- ThreadingProj/ThreadingProj/Program.cs
+++ ThreadingProj/ThreadingProj/Program.cs
@@ -5,6 +5,8 @@ namespace ThreadingProj
 {
     internal class Program
     {
+        private bool _done;
+        
         public static void Main(string[] args)
         {
             //separate thread
@@ -21,15 +23,43 @@ namespace ThreadingProj
             }
 
             Console.WriteLine("\n");
-            
             //put thread to sleep -> NOTE: if this isn't included, a block of "x"'s would've print in t2's thread
-            Thread.Sleep(1000); //1s
+            Thread.Sleep(500); //.5s
 
             //Joining threads -> wait for another to end
-            Thread t2 = new Thread(Go);
+            Thread t2 = new Thread(WriteSlash);
             t2.Start();
             t2.Join(); //wait for it to end
             Console.WriteLine("Thread t2 has ended!");
+            
+            Console.WriteLine("\n");
+            Thread.Sleep(500);
+            
+            //Local State - each thread has its own mem stack
+            //this will perform the delegate twice
+            new Thread(WriteQm).Start(); //new thread
+            WriteQm(); //on main
+            
+            Console.WriteLine("\n");
+            Thread.Sleep(500);
+            
+            //Shared state - this will only write done once, they shared the _done var
+            Program shared = new Program();
+            new Thread(shared.FlipTheSwitch).Start();
+            shared.FlipTheSwitch();
+            
+            //shared state with lambda
+            bool isDone = false;
+            ThreadStart action = () =>
+            {
+                if (!isDone)
+                {
+                    isDone = true;
+                    Console.WriteLine("Is Done!");
+                }
+            };
+            new Thread(action).Start();
+            action();
         }
 
         private static void WriteY()
@@ -41,9 +71,22 @@ namespace ThreadingProj
             }
         }
 
-        private static void Go()
+        private static void WriteSlash()
         {
             for (int i = 0; i < 100; i++) Console.Write("/");
+        }
+
+        private static void WriteQm()
+        {
+            for (int i = 0; i < 5; i++) Console.Write("?");
+            Console.Write("-");
+        }
+
+        private void FlipTheSwitch()
+        {
+            if (_done) return;
+            _done = true;
+            Console.WriteLine("Done!");
         }
     }
 }
