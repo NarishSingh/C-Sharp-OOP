61ecbdc96734d5e26e3d43d2e77f2652ce90817e
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-07-30 13:24:44 -0400

    Starting LINQ practice project

---
+++ LinqPractice/LinqPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqPractice
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] names = {"Tom", "Dick", "Harry", "Mary", "Jay"};
+
+            //Select the shortest names = order by length so we can select the shortest length, which will be first
+            IEnumerable<string> shortestNames = names.Where(
+                n => n.Length == names.OrderBy(n2 => n2.Length)
+                    .Select(n2 => n2.Length)
+                    .First()
+            );
+            Console.WriteLine($"Shortest Names: {string.Join(", ", shortestNames)}");
+            
+            //or use .Min(this, selector)
+            IEnumerable<string> shortestNames2 = names.Where(n => n.Length == names.Min(n2 => n2.Length));
+            Console.WriteLine($"Shortest Names (aggregate version): {string.Join(", ", shortestNames2)}");
+
+            Console.WriteLine("-------");
+            
+            
+        }
+    }
+}
